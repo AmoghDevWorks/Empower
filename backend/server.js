a563f0add683d260d5c5d0605a03b9e0569454eb
Bharath s <devbharu04@gmail.com>
2024-12-26 21:53:32 +0530

    added sekelton for

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express =require('express');
+
+
+const app=express();
+
+app.get('/',(req,res)=>{
+
+
+})
+
+app.get('/signup',(req,res)=>{
+
+
+})
+
+app.get('/signup',(req,res)=>{
+
+
+})
+
